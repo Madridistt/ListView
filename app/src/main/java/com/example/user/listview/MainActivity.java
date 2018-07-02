a5b8e39f367c23dbb41c0ebf7c337024c5099b81
Beks <beknazark@inbox.ru>
2018-07-02 16:46:59 +0600

    Initial commit

---
+++ app/src/main/java/com/example/user/listview/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.user.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    String [] names = new String [] { "Windows","Linux", "Apple","MacOS","Samsung","Iphone","Google Play","App Store"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+  // Находим список по идентификатору
+
+    }
+}
